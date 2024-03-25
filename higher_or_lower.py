0fdcd7cd5c988afda616f1e7b3d6c56ef00dd1dd
LydiaStoneKonstanski <yourname@example.com>
2024-03-24 22:14:28 -0400

    FINISHED

--- higher_or_lower.py
+++ higher_or_lower.py
@@ -1,18 +1,21 @@
-#A function that asks the user to provide a number between 0 and 10.
+
 from random import randrange
 
 #Helper Functions
-#function to intitiate game
-def number_guess_game():
+
+def number_guess_game() -> int:
+    '''A function that asks the user to provide a number between 0 and 10 to initiate a game.'''
     user_guess = int(input('Can you guess my number between 1 and 10?\n'))
     return user_guess
 
-#function to generate and return a number
-def random_number_generator():
+
+def random_number_generator() -> int:
+    '''function to generate and return a number'''
     return randrange(10)
 
-#function to compare number to guess and provide feedback
+
 def number_comparison(random_number, user_guess):
+    '''function to compare number to guess and provide feedback'''
     if random_number > user_guess:
         print('My number was ' + str(random_number) + '. ' + str(user_guess) + ' was too low!\n')
     elif random_number < user_guess:
@@ -20,12 +23,12 @@ def number_comparison(random_number, user_guess):
     elif random_number == user_guess:
         print('My number was ' + str(random_number) + '. ' + str(user_guess) + ' was correct!\n')
 
-#Main function calls the helpers in order
+
 def main():
+    '''Main function calls the helpers in order'''
     random_number = random_number_generator()
     user_guess = number_guess_game()
     number_comparison(random_number, user_guess)
 
-#calls main to run the program
 main()
 
